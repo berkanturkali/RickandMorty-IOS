bc4c6b0fff3bfa62239a5dcea37dd1d9951e03fc
Berkan Türkali <berkanturkali@outlook.com>
2024-09-01 19:32:52 +0300

    add LocationService

--- RickAndMorty/Model/locations/LocationResponse.swift
+++ RickAndMorty/Model/locations/LocationResponse.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct LocationResponse {
+struct LocationResponse: Codable, Identifiable {
     let id: Int?
     let name: String?
     let type: String
--- RickAndMorty/Service/location/LocationService.swift
+++ RickAndMorty/Service/location/LocationService.swift
@@ -12,7 +12,17 @@ struct LocationService {
     
     private init() {}
     
-    func fetchLocations() async throws -> BaseApiResponse<String> {
-        
+    func fetchLocations() async throws -> BaseApiResponse<LocationResponse> {
+        let urlString = Constants.locationsEndpoint
+        guard let url = URL(string: urlString) else {
+            throw NetworkError.invalidURL(urlString)
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let locations = try JSONDecoder().decode(BaseApiResponse<LocationResponse>.self, from: data)
+            return locations
+        } catch {
+            throw NetworkError.requestFailed(error.localizedDescription)
+        }
     }
 }
