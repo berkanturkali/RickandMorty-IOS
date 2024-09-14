a90f4660227dbb1220cabbd89da5245cbed523ff
Berkan Türkali <berkanturkali@outlook.com>
2024-09-14 17:42:53 +0300

    enable swipe to back gesture

--- RickAndMorty/RickAndMortyApp.swift
+++ RickAndMorty/RickAndMortyApp.swift
@@ -29,4 +29,16 @@ struct RickAndMortyApp: App {
             fatalError("Could not configure the container.")
         }
     }
+    
+}
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
 }
